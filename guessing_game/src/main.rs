9410fc28ed9b60c95281592cccdfadcc05096ef6
Bofulo <newtonombese1@gmail.com>
2026-04-05 10:05:56 +0300

    Setting up the secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -13,8 +19,8 @@ fn main() {
 
     println!("You guessed: {}", guess);
 
-    let x = 5;
-    let y = 10;
+    // let x = 5;
+    // let y = 10;
 
-    println!("x = {x} and y + 2 = {}", y + 2);
+    // println!("x = {x} and y + 2 = {}", y + 2);
 }
