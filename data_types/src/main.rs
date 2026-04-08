eaf03a36375e9807d5e81f9f6b5ecbc8576d2bbb
Bofulo <newtonombese1@gmail.com>
2026-04-08 11:00:55 +0300

    Working on testing the unassigned data type

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -2,5 +2,9 @@ fn main() {
     // let guess: u32 = "Newton".len().try_into().expect("Not a number!");
     let guess: u32 = "42".parse().expect("Not a number!");
 
+    let another_guess: i32 = "42".parse().expect("Not a number!");
+
     println!("The guess is: {guess}");
+
+    println!("The another guess is: {another_guess}");
 }
