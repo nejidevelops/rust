03d20d1c9d5dfe5eacedfad893ee202238809fbc
Bofulo <newtonombese1@gmail.com>
2026-04-08 11:04:16 +0300

    Working on testing the unassigned data type

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -7,4 +7,28 @@ fn main() {
     println!("The guess is: {guess}");
 
     println!("The another guess is: {another_guess}");
+
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    // Addition
+    let sum = 5 + 10;
+    println!("The sum is: {sum}");
+
+    // Subtraction
+    let difference = 95.5 - 4.3;
+    println!("The difference is: {difference}");
+
+    // Multiplication
+    let product = 4 * 30;
+    println!("The product is: {product}");
+
+    // Division
+    let quotient = 5.0 / 2.0;
+    println!("The quotient is: {quotient}");
+
+    // Remainder
+    let remainder = 5 % 2;
+    println!("The remainder is: {remainder}");
 }
