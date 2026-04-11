975f548083a9b5a082485ee9a4f4a3144f00dee6
Bofulo <newtonombese1@gmail.com>
2026-04-11 13:14:47 +0300

    Learning control flow and if statement

---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+C:\Users\User\Desktop\learning\hello-world-rust\control_flow\target\debug\control_flow.exe: C:\Users\User\Desktop\learning\hello-world-rust\control_flow\src\main.rs
---
+++ control_flow/target/debug/deps/control_flow.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\learning\hello-world-rust\control_flow\target\debug\deps\control_flow.d: src\main.rs
+
+C:\Users\User\Desktop\learning\hello-world-rust\control_flow\target\debug\deps\control_flow.exe: src\main.rs
+
+src\main.rs:
