a8b1a673881cc0534f047778122d10ff1eed2670
Bofulo <newtonombese1@gmail.com>
2026-04-11 12:50:49 +0300

    Successful learning of functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,16 @@ fn main() {
     another_function(5);
 
     print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x: i32) {
@@ -13,4 +23,8 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
