0f4bcbfc25d5b5f58d3c87dd67ec1b0d211a84dd
Bofulo <newtonombese1@gmail.com>
2026-04-12 09:44:33 +0300

    Working on more loops and control flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,45 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
+    if number != 0 {
         println!("Condition was true");
     } else {
         println!("Condition was false");
     }
+
+
+    let number = 16;
+
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    // let number = if condition { 5 } else { "six" };
+
+    println!("The value of number is: {number}");
+
+    loop {
+        println!("again!");
+        break;
+    }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;  
+        }
+    };
+
+    println!("The result is {result}");
 }
