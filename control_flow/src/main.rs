b07dc21bd187a79b49c5da1904672f97a014e612
Bofulo <newtonombese1@gmail.com>
2026-04-13 10:02:38 +0300

    Working on the control flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -42,4 +42,51 @@ fn main() {
     };
 
     println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("Count = {count}");
+        
+        let mut remaining = 10;
+
+        loop {
+            println!("Remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'counting_up;
+            }
+
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a {
+        println!("The value is: {element}");
+    }
 }
